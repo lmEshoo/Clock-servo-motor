37df534e81b58ccd3f075671358f59f35b90fec3
Lini Mestar <lenyrobin30@gmail.com>
2013-02-03 18:54:53 -0800

    gjhgj

---
+++ clock.ino
@@ -0,0 +1,20 @@
+// Clock
+// by Lini Mestar 
+
+#include <Servo.h> 
+ 
+Servo myClockServo;
+int SecPos = 0;
+
+void setup() 
+{ 
+  myClockServo.attach(11);
+} 
+
+void loop() 
+{ 
+   for(SecPos = 180; SecPos >= 1; SecPos -=5){                                
+    myClockServo.write(SecPos);
+    delay(1000);
+  }
+} 
